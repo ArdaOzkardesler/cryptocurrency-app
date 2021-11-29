bf1c9c595c5a5be6498299e44c0443f8b9380f4b
Arda Ozkardesler <aozkardesler@gmail.com>
2021-11-29 16:18:53 +0300

    footer updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <h4>&copy; 2021 Cryptomania. All rights reserved</h4>
+      <h4>
+        &copy; 2021 Cryptomania. All rights reserved. Data provided by{" "}
+        <a href="https://www.coingecko.com">CoinGecko</a>
+      </h4>
     </div>
   );
 }
