8d7d8a309bc4dbbc993bd90e3ed4c21d5416c1cf
Arda Ozkardesler <aozkardesler@gmail.com>
2021-11-29 17:18:08 +0300

    footer link rearranged

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,13 @@ function Footer() {
     <div>
       <h4>
         &copy; 2021 Cryptomania. All rights reserved. Data provided by{" "}
-        <a href="https://www.coingecko.com">CoinGecko</a>
+        <a
+          href="https://www.coingecko.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CoinGecko
+        </a>
       </h4>
     </div>
   );
